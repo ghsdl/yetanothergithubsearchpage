d540ceebc0086388d84692154bc76e81aeb62e2c
ghsdl <maeva.ghsdl@gmail.com>
2022-01-21 21:08:26 +0100

    :lipstick: header + css
    header + css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import logo from '../../assets/images/logo.png';
 
 import './Header.scss';
 
 const Header = () => {
-  return <header className='header'>Header</header>;
+  return (
+    <header className='header'>
+      <img src={logo} alt='GitHub logo' className='header__logo' />
+    </header>
+  );
 };
 
 export default Header;
