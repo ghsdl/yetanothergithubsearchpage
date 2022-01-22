a6db77d19897425e8f8199731823d1a421370f3d
ghsdl <maeva.ghsdl@gmail.com>
2022-01-22 11:16:27 +0100

    :memo: message feature
    message feature

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
 import './Message.scss';
 
 const Message = () => {
-  return <div className='message'>Message</div>;
+  return (
+    <div className='message'>
+      <h1 className='message__content'>1322654654 results. Check them out.</h1>
+    </div>
+  );
 };
 
 export default Message;
