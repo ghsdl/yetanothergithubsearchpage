128af65ab42a0d15291e600dc62c184a51846ff2
ghsdl <maeva.ghsdl@gmail.com>
2022-01-22 10:18:15 +0100

    :pushpin: search bar component with react rainbow
    search bar component with react rainbow + css fix

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
-
-import './SearchBar.scss';
+import { Input } from 'react-rainbow-components';
+import { BiSearchAlt } from 'react-icons/bi';
+import styles from './SearchBar.scss';
 
 const SearchBar = () => {
-  return <div className='header'>SearchBar</div>;
+  return (
+    <div className='search'>
+      <div className='rainbow-align-content_center rainbow-p-vertical_x-large rainbow-flex_wrap'>
+        <Input
+          type='search'
+          label='application component search'
+          hideLabel
+          className='rainbow-p-around_medium search__input'
+          style={styles}
+          placeholder='Search for repositories or users'
+          isCentered
+          iconPosition='right'
+          icon={<BiSearchAlt className='rainbow-color_gray-3 search__icon' />}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SearchBar;
