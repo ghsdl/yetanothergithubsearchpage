65bb8a78256e4116394bde30bfbecae5422b4876
ghsdl <maeva.ghsdl@gmail.com>
2022-02-05 20:21:09 +0100

    :test_tube: card style for repositories list

--- src/components/RepositoriesList/Repositories.jsx
+++ src/components/RepositoriesList/Repositories.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { string } from 'prop-types';
 import { Card } from 'react-rainbow-components';
+import { HiUserCircle } from 'react-icons/hi';
 
 const Repositories = ({ imageURL, name, user, description }) => {
   return (
-    <div className='repositories'>
-      <Card>
-        <img src={imageURL} className='' alt='github' />
-        <header>{name}</header>
-        <span>{user}</span>
-        <p>{description}</p>
-      </Card>
-    </div>
+    <Card className='repositories'>
+      <div>
+        <a href={imageURL}>
+          <img src={imageURL} alt='github' className='repositories__image' />
+        </a>
+      </div>
+      <h1 className='repositories__name'>{name}</h1>
+      <p className='repositories__description'>{description}</p>
+      <div className='repositories__border'></div>
+      <footer className='repositories__footer'>
+        <HiUserCircle className='repositories__icon' />
+        <p className='repositories__user'>{user}</p>
+      </footer>
+    </Card>
   );
 };
 
